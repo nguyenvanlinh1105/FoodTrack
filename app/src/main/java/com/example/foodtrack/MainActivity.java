f379d07d7cccf76e81625fa5951424d11851a52b
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-09-29 19:44:29 +0700

    commit change

--- app/src/main/java/com/example/foodtrack/MainActivity.java
+++ app/src/main/java/com/example/foodtrack/MainActivity.java
@@ -7,18 +7,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.foodtrack.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+     ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+//        setContentView(R.layout.activity_main);
+
+        //đoạn này sau này sẽ chuyển thành new home_fragment nha
+        ReplaceFragment(new profile_fragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.home){
+
+            } else if (item.getItemId() == R.id.explore) {
+                
+            } else if (item.getItemId() == R.id.cart) {
+                
+            } else if (item.getItemId() == R.id.favorite) {
+
+            } else if (item.getItemId() == R.id.account) {
+                ReplaceFragment(new profile_fragment());
+            }
+            return true;
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void ReplaceFragment(Fragment fragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frameLayout, fragment);
+        ft.commit();
     }
 }
