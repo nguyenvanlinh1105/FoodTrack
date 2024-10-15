41777a5460f9ea46e041bd7fd58df36d4c875db3
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-10-15 16:49:22 +0700

    add API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,8 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation ("com.github.bumptech.glide:glide:4.14.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.14.0")
+
+    implementation ("com.google.code.gson:gson:2.8.9")
+    implementation ("com.squareup.retrofit2:retrofit:2.1.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.1.0")
 }
---
+++ app/src/main/java/com/example/foodtrack/API/APIService.java
@@ -0,0 +1,13 @@
+package com.example.foodtrack.API;
+
+import retrofit2.Retrofit;
+
+public interface APIService {
+
+
+//    linkAPI:
+    String url ="";
+   // APIService API_SERVICE = new Retrofit.Builder().baseUrl(url);
+}
+
+
