75394b5723a9b2c4558278fcfbbe9764460cf271
Ngọc Tú Anh Nguyễn <kizunaninosete@gmail.com>
2024-10-15 15:53:59 +0700

    Updated myCard

--- app/src/main/java/com/example/foodtrack/Adapter/mycard_adapter.java
+++ app/src/main/java/com/example/foodtrack/Adapter/mycard_adapter.java
@@ -1,27 +1,32 @@
 package com.example.foodtrack.Adapter;
 
 import android.content.Context;
-import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.foodtrack.R;
 
 import java.util.ArrayList;
 
-public class card_adapter extends BaseAdapter {
+public class mycard_adapter extends BaseAdapter {
 
     Context context;
     private ArrayList<Integer> imgs;
     private LayoutInflater inflater;
+    private int selectedPosition = -1;
 
-    public card_adapter(Context context, ArrayList<Integer> imgs) {
+    public mycard_adapter(Context context, ArrayList<Integer> imgs) {
         this.context = context;
         this.imgs = imgs;
         inflater = LayoutInflater.from(context);
+    }
+
+    public void setSelectedPosition(int position) {
+        this.selectedPosition = position;
+        notifyDataSetChanged();
     }
 
     @Override
@@ -42,13 +47,21 @@ public class card_adapter extends BaseAdapter {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         if (view == null) {
-            view = inflater.inflate(R.layout.fragment_card_item, viewGroup, false);
+            view = inflater.inflate(R.layout.fragment_mycard_item, viewGroup, false);
 
         }
+        LinearLayout container = (LinearLayout)view.findViewById(R.id.card_item_container);
+        LinearLayout imgBg = (LinearLayout) view.findViewById(R.id.card_img_item);
 
-        ImageView img = (ImageView) view.findViewById(R.id.card_img_item);
+        imgBg.setBackgroundResource(imgs.get(i));
 
-        img.setImageResource(imgs.get(i));
+        if (i == selectedPosition) {
+            container.setBackgroundResource(R.drawable.selected_card_bg);
+        } else {
+            container.setBackgroundResource(0);
+        }
+
+
 
         return view;
     }
--- app/src/main/java/com/example/foodtrack/Fragment/cardItem.java
+++ app/src/main/java/com/example/foodtrack/Fragment/cardItem.java
@@ -7,11 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.example.foodtrack.R;
-
-import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -70,6 +67,6 @@ public class cardItem extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card_item, container, false);
+        return inflater.inflate(R.layout.fragment_mycard_item, container, false);
     }
 }
