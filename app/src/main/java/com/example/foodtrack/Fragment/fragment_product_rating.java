72ab1cdac51524cb6d313f69c724236640bfb0ee
Ngọc Tú Anh Nguyễn <kizunaninosete@gmail.com>
2024-10-30 22:53:17 +0700

    Undo cmt

--- app/src/main/java/com/example/foodtrack/Fragment/fragment_product_rating.java
+++ app/src/main/java/com/example/foodtrack/Fragment/fragment_product_rating.java
@@ -73,7 +73,7 @@ public class fragment_product_rating extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-//        initializeData();
+        initializeData();
     }
 
     private void initializeData(){
@@ -107,18 +107,18 @@ public class fragment_product_rating extends Fragment {
         View view = inflater.inflate(R.layout.fragment_product_rating, container, false);
 
         Mapping(view);
-//        LinearLayoutManager layoutManager
-//                = new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false);
-//        rv_product_rating.setLayoutManager(layoutManager);
-//        recyclerView_product_rating_adapter adapter = new recyclerView_product_rating_adapter(getContext(), binhLuanList);
-//        rv_product_rating.setAdapter(adapter);
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false);
+        rv_product_rating.setLayoutManager(layoutManager);
+        recyclerView_product_rating_adapter adapter = new recyclerView_product_rating_adapter(getContext(), binhLuanList);
+        rv_product_rating.setAdapter(adapter);
         ControlButton();
         return view;
     }
 
     private void Mapping(View view){
         btn_back = (ImageView) view.findViewById(R.id.btn_back_product_rating);
-//        rv_product_rating = (RecyclerView) view.findViewById(R.id.recyclerView_product_rating);
+        rv_product_rating = (RecyclerView) view.findViewById(R.id.recyclerView_product_rating);
     }
 
     private void ControlButton(){
