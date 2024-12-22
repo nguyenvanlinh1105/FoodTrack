42c69b9b931cbe1a30628e572eb293eea228825d
Lethal15004 <Phamvahuy2004@gmail.com>
2024-12-22 11:05:27 +0700

    Add tinymce

--- copy-dir.js
+++ copy-dir.js
@@ -1,4 +1,5 @@
 const fs = require('fs-extra');
+const path = require('path');
 
 // Async with promises:
 fs.copy('views', 'dist/views')
@@ -7,5 +8,11 @@ fs.copy('views', 'dist/views')
 
 // Async with promises:
 fs.copy('public', 'dist/public')
+  .then(() => console.log('success!'))
+  .catch(err => console.error(err))
+
+// Async with async/await:
+const nodeModules=path.join(__dirname, 'node_modules', 'tinymce');
+fs.copy(nodeModules, 'dist/node_modules/tinymce')
   .then(() => console.log('success!'))
   .catch(err => console.error(err))
